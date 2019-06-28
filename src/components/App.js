16ac4731570e3ed9a019c586ecfaf4a344eb171e
Maciej Nowak <maciej.nowak@billennium.com>
2019-06-28 15:19:52 +0200

    App component doesnt keep track of the state anymore

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
-/* eslint-disable react/jsx-no-bind */
 import React, { Component } from 'react';
 
 import './App.css';
 import Staff from './Staff';
 import Control from './Control';
-import store from '../store';
 
 class App extends Component {
     componentDidMount() {
@@ -14,22 +12,12 @@ class App extends Component {
         //     .then(res => console.log('here here', res));
     }
 
-    changeHandler = (name, val) => {
-        store.dispatch({ type: 'SET_FIELD', field: name, value: val });
-        this.forceUpdate();
-    }
-
     render() {
-        const state = store.getState();
         return (
             <div className="App">
-                <Staff
-                    clef={state.clef}
-                    timeSig={`${state.beatsNum}/${state.beatsType}`}
-                    keySig={state.keySig}
-                    beatsNum={parseInt(state.beatsNum, 10)}
-                    beatsType={parseInt(state.beatsType, 10)} />
-                <Control update={this.changeHandler.bind(this)} />
+                <Staff id="0" />
+                <Control
+                    id="0" />
             </div>
         );
     }
