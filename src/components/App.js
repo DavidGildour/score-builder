364cb2bda106c4c1ed91c552afea60e03aaa79cc
DavidGildour <davidgildour@gmail.com>
2019-07-10 15:03:55 +0200

    removed note indication from App due to not handling stave position changes

--- src/components/App.js
+++ src/components/App.js
@@ -6,26 +6,13 @@ class App extends Component {
     state = {
             x: 0,
             y: 0,
-            note: null,
         }
 
     handleMouseMove = (e) => {
         const curX = e.pageX;
         const curY = e.pageY;
-        let note;
 
-        if (curY <= 140) note = 'F/5';
-        else if (curY > 140 && curY <= 146 ) note = 'E/5';
-        else if (curY > 146 && curY <= 150 ) note = 'D/5';
-        else if (curY > 150 && curY <= 156 ) note = 'C/5';
-        else if (curY > 156 && curY <= 160 ) note = 'B/4';
-        else if (curY > 160 && curY <= 166 ) note = 'A/4';
-        else if (curY > 166 && curY <= 170 ) note = 'G/4';
-        else if (curY > 170 && curY <= 176 ) note = 'F/4';
-        else if (curY > 176 && curY <= 180 ) note = 'E/4';
-        else if (curY > 180 ) note = 'D/4';
-
-        this.setState({x: curX, y: curY, note: note})
+        this.setState({x: curX, y: curY})
     }
 
     render() {
@@ -41,12 +28,11 @@ class App extends Component {
                     </ul>
                     </div>
                 </nav>
-                <StaffContainer id="0" />
                 <div>
                     X: {this.state.x}&nbsp;
                     Y: {this.state.y}&nbsp;
-                    Note: {this.state.note}
                 </div>
+                <StaffContainer id="0" />
             </div>
         );
     }
