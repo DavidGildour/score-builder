599818f80b3418536d8e373e0a700f8e60523dd2
DavidGildour <davidgildour@gmail.com>
2019-07-11 12:05:58 +0200

    removed cursor position indicators from App which allowed for transforming it to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,18 @@ import React, { Component } from 'react';
 
 import StaffContainer from './StaffContainer';
 
-class App extends Component {
-    state = {
-            x: 0,
-            y: 0,
-        }
-
-    handleMouseMove = (e) => {
-        const curX = e.pageX;
-        const curY = e.pageY;
-
-        this.setState({x: curX, y: curY})
-    }
-
-    render() {
-        return (
-            <div id="main" className="App" onMouseMove={this.handleMouseMove}>
-                <nav>
-                    <div className="nav-wrapper green accent-4">
-                    <a href="/" className="brand-logo">Score Builder</a>
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="/">Nice</a></li>
-                        <li><a href="/">Navbar</a></li>
-                        <li><a href="/">Huh?</a></li>
-                    </ul>
-                    </div>
-                </nav>
-                <div>
-                    X: {this.state.x}&nbsp;
-                    Y: {this.state.y}&nbsp;
-                </div>
-                <StaffContainer id="0" />
+export default () => (
+    <div id="main" className="App">
+        <nav>
+            <div className="nav-wrapper green accent-4">
+            <a href="/" className="brand-logo">Score Builder</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="/">Nice</a></li>
+                <li><a href="/">Navbar</a></li>
+                <li><a href="/">Huh?</a></li>
+            </ul>
             </div>
-        );
-    }
-}
-
-export default App;
+        </nav>
+        <StaffContainer id="0" />
+    </div>
+);
