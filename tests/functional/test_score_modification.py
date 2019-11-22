8efed6ecbe50be70ab31723d0cc7bfd3d1667d51
DavidGildour <davidgildour@gmail.com>
2019-11-22 15:41:31 +0100

    expanded tests a little

--- tests/functional/test_score_modification.py
+++ tests/functional/test_score_modification.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -9,6 +10,19 @@ def with_wait(test):
         wait = WebDriverWait(driver, 5)
         test(driver, wait)
     return wrapper
+
+
+def open_scores_modal(driver, wait):
+    user_menu = driver.find_element_by_id('user-opt')
+    ActionChains(driver).move_by_offset(1, 1).move_to_element(user_menu).perform()
+    dropdown = wait.until(
+        EC.element_to_be_clickable((By.ID, 'user-dropdown'))
+    )
+    dropdown.find_elements_by_tag_name('a')[1].click()
+
+    return wait.until(
+        EC.presence_of_element_located((By.ID, 'scores'))
+    )
 
 
 @with_wait
@@ -24,7 +38,7 @@ def test_adding_new_score(driver, wait):
         EC.presence_of_element_located((By.CSS_SELECTOR, '.score-name input#name'))
     )
 
-    before =score_name.get_attribute('value')
+    before = score_name.get_attribute('value')
 
     wait.until_not(EC.presence_of_element_located((By.CLASS_NAME, 'info-box')))
     add_button = wait.until(
@@ -49,4 +63,31 @@ def test_changing_score_name(driver, wait):
 
     wait.until(
         EC.text_to_be_present_in_element((By.CLASS_NAME, 'info-box'), 'Successfully added new score')
+    )
+    wait.until_not(
+        EC.presence_of_element_located((By.CLASS_NAME, 'info-box'))
+    )
+
+    score_list = open_scores_modal(driver, wait).find_element_by_tag_name('ul')
+
+    score_names = \
+        (col.find_element_by_css_selector('div.col.s7').text for col in score_list.find_elements_by_tag_name('li'))
+    assert "test" in score_names
+
+    driver.find_element_by_css_selector('#scores a.modal-close').click()
+
+
+@with_wait
+def test_deleting_a_score(driver, wait):
+    score_list = open_scores_modal(driver, wait).find_element_by_tag_name('ul')
+
+    test_score = list(filter(lambda row: row.find_element_by_css_selector('div.s7').text == "test",
+                             score_list.find_elements_by_tag_name('li')))[0]
+
+    ActionChains(driver).move_to_element(test_score).click().pause(1).perform()
+
+    test_score.find_element_by_css_selector('.collapsible-body a:nth-of-type(1)').click()
+
+    wait.until(
+        EC.text_to_be_present_in_element((By.CLASS_NAME, 'info-box'), 'Successfully removed')
     )
